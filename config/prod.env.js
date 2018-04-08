b55e074f533e5a9c86799d37fb4806ee1a7b77c1
huihui <540872052@qq.com>
2018-04-08 19:30:25 +0800

    fix

--- config/prod.env.js
+++ config/prod.env.js
@@ -2,5 +2,7 @@
 module.exports = {
   NODE_ENV: '"production"',
   BASE_API: '"/api"',
-  APP_KEY: '"y745wfm8y13tv"'
+  // APP_KEY: '"y745wfm8y13tv"'   // 正式
+  APP_KEY: '"sfci50a7s4woi"'      // 测试
+
 }
